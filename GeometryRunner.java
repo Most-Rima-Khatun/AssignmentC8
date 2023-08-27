3f15c3a86d19b6690b882e025f8e6d523f7f845d
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2023-08-27 18:53:48 +0600

    Add files via upload

--- GeometryRunner.java
+++ GeometryRunner.java
@@ -1,35 +1,30 @@
-package Problem7;
+package Problem8;
 
 import java.util.Scanner;
-import java.util.concurrent.Callable;
 
 public class GeometryRunner
 {
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a radius:");
-        double radius = input.nextDouble();
+        Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter a height:");
-        double height = input.nextDouble();
+        System.out.print("Please give a Radius: ");
+        double radius = in.nextDouble();
 
-        //sphere
-        Sphere  sphere = new Sphere(radius);
-        Cone cone = new Cone(radius,height);
-        Cube cube = new Cube(height);
-        Cylinder cylinder = new Cylinder(height,radius);
-        //cube 20.2,14.2
-        System.out.printf("Volume of Cube: %20.2f\n",cube.getVolume());
-        System.out.printf("SurfaceArea of Cube: %14.2f\n",cube.getSurfaceArea());
+        System.out.print("Please give a Height: ");
+        double height = in.nextDouble();
 
-        //sphere 18.2,12.2
-        System.out.printf("Volume of Sphere: %18.2f\n",sphere.getVolume());
-        System.out.printf("SurfaceArea of Sphere: %12.2f\n",sphere.getSurfaceArea());
+        System.out.println("--------------------------------------------------------------------");
 
-        //cylinder 16.2,10.2
-        System.out.printf("Volume of Cylinder: %16.2f\n",cylinder.getVolume());
-        System.out.printf("SurfaceArea of Cylinder: %10.2f\n",cylinder.getSurfaceArea());
+        System.out.printf("Volume of a Cube: %20.2f\n", Geometry.cubeVolume(height));
+        System.out.printf("Surface Area of a Cube: %14.2f\n", Geometry.cubeSurface(height));
+        System.out.printf("Volume of a Sphere: %18.2f\n", Geometry.sphereVolume(radius));
+        System.out.printf("Surface Area of a Sphere: %12.2f\n", Geometry.sphereSurface(radius));
+        System.out.printf("Volume of a Cylinder: %16.2f\n", Geometry.cylinderVolume(radius, height));
+        System.out.printf("Surface Area of a Cylinder: %10.2f\n", Geometry.cylinderSurface(radius, height));
+        System.out.printf("Volume of a Cone: %20.2f\n", Geometry.coneVolume(radius, height));
+        System.out.printf("Surface Area of a Cone: %14.2f\n", Geometry.coneSurface(radius, height));
 
-        //cone 20.2,14.2
+        in.close();
     }
 }
